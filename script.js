418a2ec17dce706ee6d36dda9bc6097069f0f641
Daniel Berg <daniel.berg@ntig.se>
2021-10-14 16:51:15 +0200

    fixed all errors

--- script.js
+++ script.js
@@ -13,6 +13,30 @@ async function getToken() {
 // constanter
 const url = 'https://api.github.com';
 
+async function files(event) {
+  const forkid = event.target.id;
+  const main = document.querySelector('main');
+  main.innerHTML = '';
+
+  const path = await fetch(`${url}/repositories/${forkid}/contents`, { method: 'GET', headers: { Authorization: `token ${await getToken()}` } });
+  const pathFetched = await path.json();
+  console.log(pathFetched);
+  const file = JSON.stringify(pathFetched[1].download_url);
+  const htmlLink = pathFetched[1].html_url;
+
+  console.log(file);
+
+  const filecardtemplate = document.querySelector('#fork');
+  const forkClone = filecardtemplate.content.cloneNode(true);
+
+  forkClone.querySelector('.fork-title').textContent = 'Api-test';
+  forkClone.querySelector('code').textContent = file;
+  forkClone.querySelector('a').href = htmlLink;
+  forkClone.querySelector('.unit-tests').textContent = '40p';
+
+  main.appendChild(forkClone);
+}
+
 // eventlistener på input
 
 // api funktionen på eventlistener
@@ -37,8 +61,6 @@ function api(e) {
 
       clone.querySelector('#showForks').addEventListener('click', files);
       clone.querySelector('#showForks').id = id;
-      clone.querySelector('a').href = htmlLink;
-
       const addName = document.createTextNode(repoName);
       const addForks = document.createTextNode(repoForks);
       clone.querySelector('#name').appendChild(addName);
@@ -51,26 +73,3 @@ function api(e) {
 }
 
 document.querySelector('form#input').addEventListener('submit', api);
-async function files(event) {
-  const forkid = event.target.id;
-  const main = document.querySelector('main');
-  main.innerHTML = '';
-
-  const path = await fetch(`${url}/repositories/${forkid}/contents`, { method: 'GET', headers: { Authorization: `token ${await getToken()}` } });
-  const pathFetched = await path.json();
-  console.log(pathFetched);
-  const file = JSON.stringify(pathFetched[1].download_url);
-  const html_link = pathFetched[1].html_url;
-
-  console.log(file);
-
-  const filecardtemplate = document.querySelector('#fork');
-  const fork_clone = filecardtemplate.content.cloneNode(true);
-
-  fork_clone.querySelector('.fork-title').textContent = 'Api-test';
-  fork_clone.querySelector('code').textContent = file;
-  fork_clone.querySelector('a').href = html_link;
-  fork_clone.querySelector('.unit-tests').textContent = '40p';
-
-  main.appendChild(fork_clone);
-}
